a9e078ec5703b1d45675a98dfc99f7fdd46e046a
Jaime Salas Zancada <szjaime@gmail.com>
2018-08-06 23:45:02 +0200

    Added mocha and chai for tests

--- test/channelRepository.test.js
+++ test/channelRepository.test.js
@@ -1,78 +1,77 @@
-const assert = require('assert');
+const expect = require('chai').expect;
 const channelRepository = require('../src/repositories/channelRepository');
-// TODO: Add mocha, chai, sinon.
 
-const channelListHasElements = async () => {
-    await channelRepository.createChannel('foo');
-    const channelList = await channelRepository.channelList();
-    assert.equal(channelList.length > 0, true);
-    console.log('pass channelListHasElements');
-};
+describe('channelRepository', () => {
+    describe('adding new channel to empty repository', () => {
+        it('should make repository to have elements', async () => {
+            await channelRepository.createChannel('foo');
+            const channelList = await channelRepository.channelList();
+            expect(channelList.length).to.greaterThan(0);
+        });
+    });
 
-const cantCreateChannelTwice = async () => {
-    await channelRepository.createChannel('comics');
-    try {
-        await channelRepository.createChannel('comics');
-    } catch (error) {
-        assert.equal(error, 'Channel comics already exist.');
-        console.log('pass cantCreateChannelTwice');
-    }
-};
+    describe('adding twice a channel to repository', () => {
+        it('should avoid duplicated channel', async () => {
+            await channelRepository.createChannel('comics');
+            try {
+                await channelRepository.createChannel('comics');
+            } catch (error) {
+                expect(error).equals('Channel comics already exist.');
+            }
+        });
+    });
 
-const cantAddUserTwice = async () => {
-    await channelRepository.addUser('comics', 'jai');
-    try {
-        await channelRepository.addUser('comics', 'jai');
-    } catch (error) {
-        assert.equal(error, 'jai already exists in this channel.');
-        console.log('pass cantAddUserTwice');
-    }
-};
+    describe('adding twice a user to a channel repository', () => {
+        it('should avoid duplicated user', async () => {
+            await channelRepository.addUser('comics', 'jai');
+            try {
+                await channelRepository.addUser('comics', 'jai');
+            } catch (error) {
+                expect(error).equals('jai already exists in this channel.');
+            }
+        });
+    });
 
-const channelNotExistCantAddMessage = async () => {
-    try {
-        const message = {
-            channelKey: 'no exists',
-            userId: 'jai',
-            text: 'plain text'
-        };
-        await channelRepository.addMessage(message);
-    } catch (error) {
-        assert.equal(error, 'Channel: no exists or User: jai does not exist.');
-        console.log('pass channelNotExistCantAddMessage');
-    }
-};
+    describe('request messages from channel with messages', () => {
+        it('should return messages', async () => {
+            const message = {
+                channelKey: 'comics',
+                userId: 'jai',
+                text: 'batman is awesome',
+            };
+            await channelRepository.addMessage(message);
+            const messages = await channelRepository.getMessages(message.channelKey);
+            expect(messages.length).to.greaterThan(0);
+        });
+    });
 
-const userIdNotExistCantAddMessage = async () => {
-    try {
-        const message = {
-            channelKey: 'noexist',
-            userId: 'no exists',
-            text: 'plain text'
-        };
-        await channelRepository.addMessage(message);
-    } catch (error) {
-        assert.equal(error, 'Channel: noexist or User: no exists does not exist.');
-        console.log('pass userIdNotExistCantAddMessage');
-    }
-};
+    describe('add message to non existing channel', () => {
+        it('should avoid add message', async () => {
+            try {
+                const message = {
+                    channelKey: 'no exists',
+                    userId: 'jai',
+                    text: 'plain text'
+                };
+                await channelRepository.addMessage(message);
+            } catch (error) {
+                expect(error).equals('Channel: no exists or User: jai does not exist.');
+            }
+        });
+    });
 
-const getMessagesFromChannel = async () => {
-    const message = {
-        channelKey: 'comics',
-        userId: 'jai',
-        text: 'batman is awesome',
-    };
-    await channelRepository.addMessage(message);
-    const messages = await channelRepository.getMessages(message.channelKey);
-    assert.equal(messages.length > 0, true);
-};
-
-(async () => {
-    await channelListHasElements();
-    await cantCreateChannelTwice();
-    await cantAddUserTwice();
-    await channelNotExistCantAddMessage();
-    await userIdNotExistCantAddMessage();
-    await getMessagesFromChannel();
-})();
+    describe('not existing user adding a message to channel', () => {
+        it('should avoid add message', async () => {
+            try {
+                const message = {
+                    channelKey: 'noexist',
+                    userId: 'no exists',
+                    text: 'plain text'
+                };
+                await channelRepository.addMessage(message);
+            } catch (error) {
+                expect(error).equals('Channel: noexist or User: no exists does not exist.');
+            }
+        });
+    });
+});
